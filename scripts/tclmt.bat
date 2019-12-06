30b00f9deadba22768d4262c97c6f969c0b88ef4
Wojciech Kapcia <wojtek@tigase.org>
2019-12-06 20:14:55 +0100

    update licence headers to correct format; #helpdeskpr-340

--- scripts/tclmt.bat
+++ scripts/tclmt.bat
@@ -1,2 +1,20 @@
+@REM
+@REM Tigase XMPP Server Command Line Management Tool - bootstrap configuration for all Tigase projects
+@REM Copyright (C) 2004 Tigase, Inc. (office@tigase.com)
+@REM
+@REM This program is free software: you can redistribute it and/or modify
+@REM it under the terms of the GNU Affero General Public License as published by
+@REM the Free Software Foundation, version 3 of the License.
+@REM
+@REM This program is distributed in the hope that it will be useful,
+@REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+@REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@REM GNU Affero General Public License for more details.
+@REM
+@REM You should have received a copy of the GNU Affero General Public License
+@REM along with this program. Look for COPYING file in the top folder.
+@REM If not, see http://www.gnu.org/licenses/.
+@REM
+
 @echo off
 %JAVA_HOME%\bin\java -cp "libs/*;jars/tclmt-1.0.0-SNAPSHOT.jar" tigase.tclmt.Tclmt %*
--- scripts/tclmt.sh
+++ scripts/tclmt.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
+#
+# Tigase XMPP Server Command Line Management Tool - bootstrap configuration for all Tigase projects
+# Copyright (C) 2004 Tigase, Inc. (office@tigase.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. Look for COPYING file in the top folder.
+# If not, see http://www.gnu.org/licenses/.
+#
+
 CLASSPATH="`ls -d libs/*.jar 2>/dev/null | tr '\n' :`${CLASSPATH}"
 java -cp $CLASSPATH:jars/tclmt-1.0.0-SNAPSHOT.jar tigase.tclmt.Tclmt "$@"
